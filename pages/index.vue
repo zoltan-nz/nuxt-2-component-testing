64b0413aacd4995f4fd86ccac1b06222b698eab6
Zoltan <zdebre@gmail.com>
2023-07-19 08:03:01 -0400

    build: update dependencies

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <template>
-  <Tutorial />
+  <h1>Hello World</h1>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-
-  export default Vue.extend({
-    name: 'IndexPage',
-  });
+  export default {
+    name: 'IndexPage'
+  }
 </script>
